0187e8a5697b283843567cb99897951437c1f025
donne <xdong001@126.com>
2019-12-05 16:39:12 +0800

    add app_test.go

---
+++ app_test.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	expTime   = 60
+	longURL   = "https://www.baidu.com"
+	shortLink = "IFHzaO"
+)
+
+type storageMock struct {
+	mock.Mock
+}
+
+var app App
+var mockR *storageMock
+
+func (s *storageMock) Shorten(url string, exp int) (string, error) {
+	args := s.Called(url, exp)
+	return args.String(0), args.Error(1)
+}
+
+func (s *storageMock) UnShorten(eid string) (string, error) {
+	args := s.Called(eid)
+	return args.String(0), args.Error(1)
+}
+
+func (s *storageMock) ShortLinkInfo(eid string) (interface{}, error) {
+	args := s.Called(eid)
+	return args.String(0), args.Error(1)
+}
+
+func init() {
+	app = App{}
+	mockR = new(storageMock)
+	app.Init(&Env{S: mockR})
+}
+
+func TestCreateShortLink(t *testing.T) {
+	// define request body content
+	var jsonContent = []byte(`{
+		"url": "https://www.baidu.com",
+		"expire": 60
+	}`)
+
+	// create a request
+	req, err := http.NewRequest("POST", "/api/shorten", bytes.NewBuffer(jsonContent))
+	if err != nil {
+		t.Fatalf("could not create the request, error: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	// setup expectations
+	mockR.On("Shorten", longURL, expTime).Return(shortLink, nil).Once()
+	rw := httptest.NewRecorder()
+	app.Router.ServeHTTP(rw, req)
+
+	assert.Equal(t, rw.Code, http.StatusOK, "don't get the expected status code")
+
+	resp := struct {
+		ShortLink string
+	}{}
+	if err := json.NewDecoder(rw.Body).Decode(&resp); err != nil {
+		t.Fatalf("could not decode the response, error: %v", err)
+	}
+
+	assert.Equal(t, resp.ShortLink, shortLink, "don't get the expected short link")
+}
+
+func TestRedirect(t *testing.T) {
+	r := fmt.Sprintf("/%s", shortLink)
+	req, err := http.NewRequest("GET", r, nil)
+	if err != nil {
+		t.Fatalf("could not create request, error: %v", err)
+	}
+
+	mockR.On("UnShorten", shortLink).Return(longURL, nil).Once()
+	rw := httptest.NewRecorder()
+	app.Router.ServeHTTP(rw, req)
+
+	assert.Equal(t, rw.Code, http.StatusTemporaryRedirect, "don't get the expected status code")
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
-	github.com/go-redis/redis/v7 v7.0.0-beta.4 // indirect
-	github.com/pilu/go-base62 v0.0.0-20131214171223-d5f2747573ce // indirect
+	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/pilu/go-base62 v0.0.0-20131214171223-d5f2747573ce
+	github.com/stretchr/testify v1.4.0
 )
